2937cdf3384e8ee14668eb35cb67d334a57c1368
gabrielNativa <gabrielmoreira450@outlook.com>
2025-12-09 13:21:32 -0300

    Insercao de usuario com POST

--- src/main/java/com/gabrielnatividade/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielnatividade/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielnatividade.workshopmongo.domain.User;
+import com.gabrielnatividade.workshopmongo.dto.UserDTO;
 import com.gabrielnatividade.workshopmongo.repository.UserRepository;
 import com.gabrielnatividade.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 	    return repo.findById(id)
 	            .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
