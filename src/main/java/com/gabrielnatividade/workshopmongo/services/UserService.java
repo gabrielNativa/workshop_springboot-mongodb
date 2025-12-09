e9c6a699f6f1f42c558d9f6a93636c451be43b73
gabrielNativa <gabrielmoreira450@outlook.com>
2025-12-09 13:36:12 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/gabrielnatividade/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielnatividade/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabrielnatividade.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,18 @@ public class UserService {
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		 User newObj = repo.findById(obj.getId())
+		.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 	public User fromDTO(UserDTO objDto) {
