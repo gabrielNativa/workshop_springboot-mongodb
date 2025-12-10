0d2d08ae56ff22b9d2d99b7a3b561f1852b0a951
gabrielNativa <gabrielmoreira450@outlook.com>
2025-12-10 15:02:29 -0300

    Endpoint para retornar os post de um usuario

--- src/main/java/com/gabrielnatividade/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielnatividade/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabrielnatividade.workshopmongo.domain.Post;
 import com.gabrielnatividade.workshopmongo.domain.User;
 import com.gabrielnatividade.workshopmongo.dto.UserDTO;
 import com.gabrielnatividade.workshopmongo.services.UserService;
@@ -58,6 +59,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping (value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
