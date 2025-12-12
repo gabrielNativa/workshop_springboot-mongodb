f0017d4735e51d023a856d16f30ad05c8bc2e59d
gabrielNativa <gabrielmoreira450@outlook.com>
2025-12-12 14:04:54 -0300

    Consulta com varios criterios

--- src/main/java/com/gabrielnatividade/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrielnatividade/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.gabrielnatividade.workshopmongo.repository;
 
+
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,7 +9,7 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gabrielnatividade.workshopmongo.domain.Post;
-import com.gabrielnatividade.workshopmongo.domain.User;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
@@ -18,4 +20,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	@Query("{ $and: [ {date: {$gte: ?1}  }, {date: {$lte: ?2} }, { $or: [{'title': { $regex: ?0, $options: 'i' } }, {'body': { $regex: ?0, $options: 'i' } },{'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text , Date minDate, Date maxDate);
+	
 }
--- src/main/java/com/gabrielnatividade/workshopmongo/services/PostService.java
+++ src/main/java/com/gabrielnatividade/workshopmongo/services/PostService.java
@@ -2,6 +2,7 @@ package com.gabrielnatividade.workshopmongo.services;
 
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,10 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 
 
 	
